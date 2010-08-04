5da90edd00c8668d07b9d613a46bbbc086f1215c
Chris Pickel <sfiera@gmail.com>
2010-08-04 00:24:05 -0400

    Set users for remote commands.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -2,3 +2,14 @@
 
 autoload -U compinit
 compinit
+
+REMOTE_COMMANDS='(ssh|sshfs|sftp|scp|rsync)'
+REMOTE_USERS=(sfiera chpickel)
+USERS_HOSTS=(
+    sfiera@{florence,a}.sfiera.net
+    sfiera@{durendal,bighouse,bernhard,boomer,blake,bob}
+    sfiera@{courtain,hauteclaire,sauvagine}
+    chpickel@force.stwing.org
+)
+zstyle ":completion:*:*:$REMOTE_COMMANDS:*:users" users $REMOTE_USERS
+zstyle ":completion:*:my-accounts" users-hosts $USERS_HOSTS
