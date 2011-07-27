16fc3e71945f6029941c20cab05bfed628539701
Chris Pickel <sfiera@gmail.com>
2011-07-27 13:19:45 -0400

    Add one-char filesystem navigation commands.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,5 +3,27 @@
 alias ls="/bin/ls -F"
 alias si="sort | uniq"
 
+alias d="dirs -v"
+
+c() {
+    cd "$@" && d
+}
+
+p() {
+    pushd "$@" >/dev/null && d
+}
+
+o() {
+    popd "$@" >/dev/null && d
+}
+
+r() {
+    case "$#" in
+        0) p "+1" ;;
+        1) p "+$1" ;;
+        *) echo "usage: $0 N" >&2 ;;
+    esac
+}
+
 # if $DARWIN
 alias preview="open -f -a Preview"
