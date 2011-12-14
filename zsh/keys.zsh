c421e5da9f95b3d4cfc7b6d495440a20209d20da
Chris Pickel <sfiera@sfzmail.com>
2011-12-14 15:46:34 -0500

    Sync vim commands to zsh.

--- zsh/keys.zsh
+++ zsh/keys.zsh
@@ -6,12 +6,13 @@ bindkey -v
 
 # Bindings in insert mode.
 bindkey -M viins -rp '^['
-bindkey -M viins '^[[3~' delete-char
+bindkey -M viins '^[[3~' vi-cmd-mode
 bindkey -M viins '^[[A' up-line-or-history
 bindkey -M viins '^[[B' down-line-or-history
 bindkey -M viins '^[[C' forward-char
 bindkey -M viins '^[[D' backward-char
 
+bindkey -M vicmd '^[[3~' beep
 bindkey -M vicmd j backward-char
 bindkey -M vicmd k down-line-or-history
 bindkey -M vicmd l up-line-or-history
@@ -21,3 +22,4 @@ bindkey -M vicmd \' vi-repeat-find
 
 # Bindings in command mode.
 bindkey -M vicmd 'u' undo
+bindkey -M vicmd 'U' redo
