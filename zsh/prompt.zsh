33b2caa4c25377f447fcc7e74eef030d4387f1a9
Chris Pickel <sfiera@google.com>
2012-01-06 17:12:04 -0500

    Dim non-git-path.  Use 256 colors.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
 MACHINE=${MACHINE-%m}
-PS_COLOR=${PS_COLOR-yellow}
+
+PS_COLOR=${PS_COLOR-100}
+DIRTY_COLOR=${DIRTY_COLOR-167}
+CLEAN_COLOR=${CLEAN_COLOR-185}
+DIM_COLOR=${DIM_COLOR-239}
 
 autoload colors zsh/terminfo
 if [[ "$terminfo[colors]" -ge 8 ]] colors
@@ -25,8 +29,8 @@ function git_dirty {
 }
 
 function tint {
-    local COLOR="%{$terminfo[bold]$fg[$1]%}"
-    local RESET="%{$terminfo[sgr0]%}"
+    local COLOR="%{\e[38;05;${1}m%}"
+    local RESET="%{\e[38;05;m%}"
     shift
     echo "$COLOR$*$RESET"
 }
@@ -34,19 +38,31 @@ function tint {
 function set_prompt {
     local NEWLINE="
 "
+    local HERE="%~"
+    HERE="${(%)HERE}"
 
     case $USER in
         root|sfiera|chpickel)   local LOCATION="$MACHINE" ;;
         *)                      local LOCATION="$USER@$MACHINE" ;;
     esac
-    PS1="$NEWLINE$(tint $PS_COLOR $LOCATION):%~"
+    PS1="$NEWLINE$(tint $PS_COLOR $LOCATION)"
 
     if git rev-parse --show-toplevel >/dev/null 2>/dev/null; then
-        if git_dirty; then
-            PS1="$PS1:$(tint red $(git_branch))"
+        GIT_PREFIX=$(git rev-parse --show-prefix)
+        GIT_PREFIX=${GIT_PREFIX%/}
+        GIT_TOPLEVEL=${HERE%${GIT_PREFIX}}
+        if [[ ( $GIT_TOPLEVEL != $HERE ) || -z $GIT_PREFIX ]]; then
+            PS1="$PS1:$(tint $DIM_COLOR $GIT_TOPLEVEL)$GIT_PREFIX"
         else
-            PS1="$PS1:$(tint yellow $(git_branch))"
+            PS1="$PS1:$HERE"
         fi
+        if git_dirty; then
+            PS1="$PS1:$(tint $DIRTY_COLOR $(git_branch))"
+        else
+            PS1="$PS1:$(tint $CLEAN_COLOR $(git_branch))"
+        fi
+    else
+        PS1="$PS1:$HERE"
     fi
 
     PS1="$PS1$NEWLINE$(tint $PS_COLOR %#) "
