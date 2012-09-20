ae084ac1ff93730ad453f194c7dc712dee123c61
Chris Pickel <sfiera@sfzmail.com>
2012-09-20 16:45:09 -0400

    zsh: don't complete wildcard ssh hosts.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -5,6 +5,6 @@ compinit
 
 REMOTE_COMMANDS='(ssh|sshfs|sftp|scp|rsync)'
 REMOTE_USERS=(sfiera chpickel)
-REMOTE_HOSTS=($(grep ^Host ~/.ssh/config | cut -d' ' -f2))
+REMOTE_HOSTS=($(grep '^Host [^*]\+ ' ~/.ssh/config | cut -d' ' -f2))
 zstyle ":completion:*" users $REMOTE_USERS
 zstyle ":completion:*" hosts $REMOTE_HOSTS
