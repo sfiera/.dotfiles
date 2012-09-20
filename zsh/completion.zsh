2943a26e59578affa4d204135dee13e34c410a45
Chris Pickel <sfiera@sfzmail.com>
2012-09-20 16:42:07 -0400

    zsh: complete hosts off of ssh/config.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -5,11 +5,6 @@ compinit
 
 REMOTE_COMMANDS='(ssh|sshfs|sftp|scp|rsync)'
 REMOTE_USERS=(sfiera chpickel)
-USERS_HOSTS=(
-    sfiera@{florence,a}.sfiera.net
-    sfiera@{durendal,bighouse,bernhard,boomer,blake,bob}
-    sfiera@{courtain,hauteclaire,sauvagine}
-    chpickel@force.stwing.org
-)
-zstyle ":completion:*:*:$REMOTE_COMMANDS:*:users" users $REMOTE_USERS
-zstyle ":completion:*:my-accounts" users-hosts $USERS_HOSTS
+REMOTE_HOSTS=($(grep ^Host ~/.ssh/config | cut -d' ' -f2))
+zstyle ":completion:*" users $REMOTE_USERS
+zstyle ":completion:*" hosts $REMOTE_HOSTS
