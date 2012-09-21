8b7362ff2c7c52f7e2959057d7d687ca59bff967
Chris Pickel <sfiera@sfzmail.com>
2012-09-21 10:38:07 -0400

    zsh: restore users-hosts completion style.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,8 +3,35 @@
 autoload -U compinit
 compinit
 
-REMOTE_COMMANDS='(ssh|sshfs|sftp|scp|rsync)'
-REMOTE_USERS=(sfiera chpickel)
-REMOTE_HOSTS=($(grep '^Host [^*]\+ ' ~/.ssh/config | cut -d' ' -f2))
-zstyle ":completion:*" users $REMOTE_USERS
-zstyle ":completion:*" hosts $REMOTE_HOSTS
+remote_users=("$LOGNAME")
+remote_hosts=()
+remote_users_hosts=()
+declare -U remote_users
+declare -U remote_hosts
+declare -U remote_users_hosts
+
+cat $ZSH/ssh/config | while read CMD VALUE UNUSED; do
+    if [[ "$CMD" == "Host" ]]; then
+        if [[ ! -z "$REMOTE_HOST" ]]; then
+            remote_users_hosts=("$LOGNAME@$REMOTE_HOST" $remote_users_hosts)
+            REMOTE_HOST=
+        fi
+        if [[ -z "${VALUE##*\**}" ]]; then
+            REMOTE_HOST=
+        else
+            REMOTE_HOST="$VALUE"
+            remote_hosts=("$REMOTE_HOST" $remote_hosts)
+        fi
+    elif [[ "$CMD" == "User" ]]; then
+        REMOTE_USER="$VALUE"
+        remote_users=("$REMOTE_USER" $remote_users)
+        if [[ ! -z "$REMOTE_HOST" ]]; then
+            remote_users_hosts=("$REMOTE_USER@$REMOTE_HOST" $remote_users_hosts)
+            REMOTE_HOST=
+        fi
+    fi
+done
+
+zstyle ":completion:*" users $remote_users
+zstyle ":completion:*" hosts $remote_hosts
+zstyle ":completion:*" users-hosts $remote_users_hosts
