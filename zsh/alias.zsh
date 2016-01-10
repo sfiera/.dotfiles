6d7113f0b5000dd9f1a568b11ac4b930e3ee7ef0
Chris Pickel <sfiera@sfzmail.com>
2016-01-09 22:00:59 -0500

    zsh: change cloak alias to %@ for ALBUMARTIST.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,7 +2,7 @@
 
 alias ls="/bin/ls -F"
 alias si="sort | uniq"
-alias cloak="cloak --path='./%g/%b/%A/%d%k%t'"
+alias cloak="cloak --path='./%g/%@/%A/%d%k%t'"
 alias suvi="sudo -e"
 alias pk="pkill -fI"
 
